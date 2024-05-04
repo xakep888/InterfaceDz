e9a886218a5696fc5572654d330800c82f3da7e6
Nikita <topolik777@mail.ru>
2024-05-04 20:23:41 +0300

    Create StudentGroup

---
+++ StudentGroup.java
@@ -0,0 +1,14 @@
+public class StudentGroup {
+    private String name;
+
+
+    public StudentGroup(String name){
+        this.name = name;
+    }
+
+    public String name(){
+        return name;
+    }
+
+
+}
